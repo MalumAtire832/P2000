62b8d2e47fbb508363f003630cff4530c4fdd505
Harjan Knapper <harjanknapper@hotmail.com>
2018-07-02 20:29:30 +0200

    Moved scrape_all and write_all to utils.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import p2000.utils as utils
 from p2000.rtlsdr import Connection, AbstractReader
-from p2000.tomzulu import Scraper, Region, Database
-from progress.bar import Bar
 from pprint import pprint
 
 
@@ -14,52 +13,8 @@ class MyReader(AbstractReader):
             print(str(line))
 
 
-def fetch_all_units():
-    result = []
-    regions = Region.all()
-    bar = Bar("Scraping units from the web", max=len(regions), fill="■", suffix='%(percent)d%%')
-    for region in regions:
-        scraper = Scraper(region)
-        units = scraper.get_units()
-        for discipline in units:
-            for unit in discipline:
-                result.append(unit)
-        bar.next()
-    bar.finish()
-    return result
-
-
-def write_all_units():
-    units = fetch_all_units()
-    writer = Database()
-    bar = Bar(message='Writing unit %(index)d of %(max)d', max=len(units), fill="■", suffix='%(percent)d%%')
-    for unit in units:
-        writer.write_unit(unit)
-        bar.next()
-    bar.finish()
-
-
-# write_all_units()
-
-# scraper = Scraper(Region.FRIESLAND)
-# landing = scraper.get_landing_page()
-# links = scraper.get_discipline_links(landing)
-# units = scraper.get_units(links[0])
-#
-# writer = Database()
-# writer.write_units(units)
-# unit = writer.find_unit("0300050")
-# units = writer.find_units("0300050")
-#
-# pprint(unit)
-# pprint(units)
-
-# for discipline in units:
-#     pprint("{0} Size = {1}".format(discipline[0].discipline, len(discipline)))
-
-
-# connection = Connection()
-# reader = MyReader()
-# reader.attach(connection)
+connection = Connection()
+reader = MyReader()
+reader.attach(connection)
 
 
--- p2000/utils.py
+++ p2000/utils.py
@@ -1,4 +1,6 @@
 import json
+from p2000.tomzulu import Scraper, Region, Database
+from progress.bar import ShadyBar
 
 
 def load_config():
@@ -8,3 +10,44 @@ def load_config():
     """
     with open('./resources/config.json') as file:
         return json.load(file)
+
+
+def unit_bar(total):
+    return ShadyBar(
+        message='Writing unit %(index)d of %(max)d',
+        max=total,
+        suffix='%(percent)d%%'
+    )
+
+
+def scrape_bar(total):
+    return ShadyBar(
+        message="Scraping units from the web",
+        max=total,
+        suffix='%(percent)d%%'
+    )
+
+
+def fetch_all_units():
+    result = []
+    regions = Region.all()
+    bar = scrape_bar(len(regions))
+    for region in regions:
+        scraper = Scraper(region)
+        units = scraper.get_units()
+        for discipline in units:
+            for unit in discipline:
+                result.append(unit)
+        bar.next()
+    bar.finish()
+    return result
+
+
+def write_all_units():
+    units = fetch_all_units()
+    writer = Database()
+    bar = unit_bar(len(units))
+    for unit in units:
+        writer.write_unit(unit)
+        bar.next()
+    bar.finish()
