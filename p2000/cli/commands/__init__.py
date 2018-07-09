301e0fcc9fa1af7d8eb21bc0bd60271fc02088ce
Harjan Knapper <harjanknapper@hotmail.com>
2018-07-09 15:35:04 +0200

    Removed CLI progress, deemed not useful for now.

--- p2000/cli/commands/__init__.py
+++
@@ -1,1 +0,0 @@
-from scrape import Scrape
--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ setup(
     name='Piton 2000',
     version='0.0.1',
     packages=find_packages(exclude=['examples', 'tests*']),
-    entry_points={
-        'console_scripts': [
-            'piton2000=p2000.cli.runner:main',
-        ],
-    },
     url='https://github.com/MalumAtire832/P2000',
     license='GPL-3.0',
     author='Harjan Knapper',
     author_email='harjan@knapper-development.nl',
     description='Piton 2000 is a service that can connect to the Dutch P2000 network to monitor the messages send by alarm centers to the Fire Departments, Police stations, Ambulance services or the KNRM.'
 )
+
+# entry_points={
+#     'console_scripts': [
+#         'piton2000=p2000.cli.runner:main',
+#     ],
+# },
