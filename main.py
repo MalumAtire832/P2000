35440bf62af35ea2d588240237821f73ac39f5b5
Harjan Knapper <harjanknapper@hotmail.com>
2018-07-10 00:06:22 +0200

    Updated requirements.txt

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-# import p2000.rtlsdr as rtlsdr
-# from p2000.rtlsdr import AbstractReader
-#
-#
-# class MyReader(AbstractReader):
-#
-#     def act(self, raw):
-#         line = self.create_line(raw)
-#         if self.is_line_blacklisted(line):
-#             print("== LINE IS BLACKLISTED ==")
-#         else:
-#             print(str(line))
-#
-#
-# connection = rtlsdr.Connection()
-# reader = MyReader()
-# reader.attach(connection)
-
-# from p2000 import Unit
-# from p2000.storage.units import Connection
-#
-#
-# connection = Connection()
-# connection.write_unit(Unit())
-
 from pprint import pprint
 
 from p2000 import Region, Discipline
