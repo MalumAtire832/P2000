c14f2d5a9fb129e7e2ff112df43abe5f22f2a948
Harjan Knapper <harjanknapper@hotmail.com>
2018-07-10 00:51:52 +0200

    Implemented methods for checking if multimon-ng and rtl-fm are installed, this fixes #1

--- p2000/utils.py
+++ p2000/utils.py
@@ -1,4 +1,9 @@
 import json
+import os
+import subprocess
+# "Program 'rtl-fm' not installed, see the Setup section in the README for more info."
+
+DEVNULL = open(os.devnull, 'w')
 
 
 def load_config():
@@ -8,3 +13,25 @@ def load_config():
     """
     with open('./resources/config.json') as f:
         return json.load(f)
+
+
+def is_rtlfm_installed():
+    try:
+        subprocess.call(["rtl_fm", "-h"], stdout=DEVNULL, stderr=subprocess.STDOUT, close_fds=True)
+        return True
+    except OSError as e:
+        if e.errno == os.errno.ENOENT:
+            return False
+        else:
+            raise
+
+
+def is_multimon_installed():
+    try:
+        subprocess.call(["rtl_fm", "-h"], stdout=DEVNULL, stderr=subprocess.STDOUT, close_fds=True)
+        return True
+    except OSError as e:
+        if e.errno == os.errno.ENOENT:
+            return False
+        else:
+            raise
